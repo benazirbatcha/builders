167039d34c5c2e5ca17678235975cc621a571a3f
benazirbatcha <benacse95@gmail.com>
2018-08-04 21:16:30 +0530

    index creation

---
+++ public_html/index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Builders </title>
+</head>
+<body>
+<form>
+	<input type="" name="">
+	<input type="" name="">
+	<input type="" name="">
+	<input type="" name="">
+	<input type="" name="">
+	<input type="submit" name="">
+</form>
+</body>
+</html>
